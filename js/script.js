72fe5d4ed30b049bccbed34e251fb61c31bc69cd
Awe <theuraniumcat@gmail.com>
2024-08-08 00:04:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,54 +13,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
     donateButton.addEventListener('click', function() {
         if (!isDonateMenuVisible) {
-            buttons.forEach(button => button.classList.add('fade-out'));
+            buttons.forEach(button => {
+                button.classList.add('fade-out');
+                setTimeout(() => {
+                    if (button !== donateButton) {
+                        button.querySelector('h2').textContent = getPaymentMethod(button);
+                    } else {
+                        button.querySelector('h2').innerHTML = 'Back &larr;';
+                    }
+                }, 500);
+            });
+
             setTimeout(() => {
                 buttons.forEach(button => {
-                    button.classList.add('hidden');
-                    button.querySelector('h2').innerHTML = button === donateButton ? 'Back &larr;' : getPaymentMethod(button);
+                    button.classList.remove('fade-out');
+                    button.classList.add('fade-in');
                 });
-                buttons.forEach(button => button.classList.remove('hidden'));
-                buttons.forEach(button => button.classList.add('fade-in'));
+
                 setTimeout(() => {
                     buttons.forEach(button => button.classList.remove('fade-in'));
                 }, 500);
-                isDonateMenuVisible = true;
             }, 500);
+
+            isDonateMenuVisible = true;
         } else {
-            buttons.forEach(button => button.classList.add('fade-out'));
+            buttons.forEach(button => {
+                button.classList.add('fade-out');
+                setTimeout(() => {
+                    button.querySelector('h2').innerHTML = getOriginalText(button);
+                }, 500);
+            });
+
             setTimeout(() => {
                 buttons.forEach(button => {
-                    button.classList.add('hidden');
-                    button.querySelector('h2').innerHTML = getOriginalText(button);
+                    button.classList.remove('fade-out');
+                    button.classList.add('fade-in');
                 });
-                buttons.forEach(button => button.classList.remove('hidden'));
-                buttons.forEach(button => button.classList.add('fade-in'));
+
                 setTimeout(() => {
                     buttons.forEach(button => button.classList.remove('fade-in'));
                 }, 500);
-                isDonateMenuVisible = false;
             }, 500);
+
+            isDonateMenuVisible = false;
         }
     });
 
     function getPaymentMethod(button) {
-        switch (button) {
-            case buttons[0]: return 'PayPal';
-            case buttons[1]: return 'Credit Card';
-            case buttons[2]: return 'Bitcoin';
-            default: return 'Unknown';
-        }
-    }
-
-    function getOriginalText(button) {
-        switch (button) {
-            case buttons[0]: return 'Telegram <span>&rarr;</span>';
-            case buttons[1]: return 'Steam <span>&rarr;</span>';
-            case buttons[2]: return 'Discord <span>&rarr;</span>';
-            case buttons[3]: return 'VK [inactive]';
-            case buttons[4]: return 'YouTube [inactive]';
-            case buttons[5]: return 'Donate <span>&rarr;</span>';
-            default: return 'Unknown';
-        }
-    }
-});
+        switch (button)
