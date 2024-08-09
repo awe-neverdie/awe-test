f1d27d94231e8caf2c46501a707aa18577337fa4
Awe <theuraniumcat@gmail.com>
2024-08-09 04:08:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const donateButton = document.querySelector('.donate-button');
+    const backButton = document.querySelector('.back-button a');
+    const buttonList = document.querySelector('.button-list');
+    const donateDetails = document.querySelector('.donate-details');
+
+    donateButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        buttonList.style.opacity = '0';
         setTimeout(function () {
-            document.querySelector('.loader-container').style.opacity = '0';
-            document.querySelector('main').style.opacity = '1';
-            document.body.style.overflowY = 'scroll';
-        }, 1000);
-    };
+            buttonList.classList.add('hidden');
+            donateDetails.classList.remove('hidden');
+            donateDetails.classList.add('visible');
+        }, 500);
+    });
+
+    backButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        donateDetails.style.opacity = '0';
+        setTimeout(function () {
+            donateDetails.classList.remove('visible');
+            donateDetails.classList.add('hidden');
+            buttonList.classList.remove('hidden');
+            buttonList.style.opacity = '1';
+        }, 500);
+    });
+});
