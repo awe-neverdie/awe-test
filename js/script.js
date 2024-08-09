ef86fc58043cf7f935799f631488949b4480b3b3
Awe <theuraniumcat@gmail.com>
2024-08-09 03:34:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,7 @@
 window.onload = function () {
-    setTimeout(function () {
-        document.querySelector('.loader-container').style.opacity = '0';
-        document.querySelector('main').style.opacity = '1';
-        document.body.style.overflowY = 'scroll';
-    }, 1000);
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-    const donateButton = document.getElementById('donate-button');
-    const backButton = document.getElementById('back-button');
-    const buttons = document.querySelectorAll('.link-button');
-    const donateMenu = document.getElementById('donate-menu');
-
-    donateButton.addEventListener('click', function() {
-        buttons.forEach(button => button.classList.add('fade-out'));
-        setTimeout(() => {
-            buttons.forEach(button => button.classList.add('hidden'));
-            donateMenu.classList.add('active', 'fade-in');
-        }, 500);
-    });
-
-    backButton.addEventListener('click', function() {
-        donateMenu.classList.remove('fade-in');
-        donateMenu.classList.remove('active');
-        buttons.forEach(button => button.classList.remove('hidden'));
-        setTimeout(() => {
-            buttons.forEach(button => button.classList.remove('fade-out'));
-        }, 500);
-    });
-});
+        setTimeout(function () {
+            document.querySelector('.loader-container').style.opacity = '0';
+            document.querySelector('main').style.opacity = '1';
+            document.body.style.overflowY = 'scroll';
+        }, 1000);
+    };
