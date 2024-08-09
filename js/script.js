d47acc1645fc5d16050f605dec4a97c7cb7150af
Awe <theuraniumcat@gmail.com>
2024-08-09 04:14:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+window.onload = function () {
+    setTimeout(function () {
+        const loader = document.querySelector('.loader-container');
+        loader.style.opacity = '0';
+        setTimeout(function () {
+            loader.style.display = 'none'; // Hide the loader completely after fade out
+            document.querySelector('main').style.opacity = '1';
+            document.body.style.overflowY = 'scroll';
+        }, 1000); // Match the duration of the fade-out transition
+    }, 1000); // Initial delay before starting the fade-out
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     const donateButton = document.querySelector('.donate-button');
     const backButton = document.querySelector('.back-button');
