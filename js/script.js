e303caebc95a36f352d516e79d67fd46d53acbc8
Awe <theuraniumcat@gmail.com>
2024-08-09 03:15:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ window.onload = function () {
             document.body.style.overflowY = 'scroll';
         }, 1000);
     };
+document.addEventListener('DOMContentLoaded', functon() {
+        const donateButton = document.getElementById('donate-button');
+        const backButton = document.getElementById('back-button');
+        const buttons = document.getElementById('.link-button');
+        const donateMenu = document.getElementById('donate-menu');
+
+        donateButton.addEventListener('click', function() {
+                buttons.forEach(button => button.classList.add('fade-out'));
+                setTimeout(() => {
+                        buttons.forEach(button => button.classList.add('hidden));
+                        donateMenu.classList.add('active', 'fade-in');
+                }, 500);
+        });
+        backButton.addEventListener('click', function() {
+                donateMenu.classList.remove('fade-in');
+                donateMenu.classList.remove('active');
+                buttons.forEach(button => button.classList.remove('hiden'));
+                setTimeout(() => {
+                        buttons.forEach(button => button.classList.remove('fade-out'));
+                }, 500);
+        });
+});
